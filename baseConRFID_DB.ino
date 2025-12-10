c0330bc3c17ce3fa35e2d269d87d97a829b8c9b1
Joshua <158305849+SlayerHide@users.noreply.github.com>
2025-12-09 18:32:33 -0600

    Code arduino RFID

---
+++ baseConRFID_DB.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+  Serial.println("Listo. Pasa una tarjeta...");
+}
+
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent()) return;
+  if (!rfid.PICC_ReadCardSerial()) return;
+
+  String uid = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    uid += String(rfid.uid.uidByte[i], HEX);
+  }
+
+  Serial.println(uid);
+
+  rfid.PICC_HaltA();
+}
